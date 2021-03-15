db8895a116caf89f8882a2a659f2bef9d3252b3f
Ilia <izagorodn@gmail.com>
2021-03-15 07:59:17 +0300

    на данный момент это все что готово, подробнее см в описании
    Готов основной каркас приложения, есть возможность добавлять сколько угодно комментов, но я к сожалению не понимаю почему отдельное поле ввода для комментариев низшего порядка не подтягивается, я НЕ сделал имитацию асинхроннх запросов на сервак, реакт-счетчик и нормальные стили, пардон что занял так много вашего времени с тестовым, я правда старался)

--- app/app.js
+++ app/app.js
@@ -1,14 +1,50 @@
-'use strict';
+let CommentsApp = angular.module('CommentsApp', [])
 
-// Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+CommentsApp.controller('commentsController', ['$scope', '$log', function ($scope) {
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  const hierarchy = (
+      data = [],
+      {idKey='id',parentKey='parentId',childrenKey='children'} = {}
+  ) => {
+    const tree = [];
+    const childrenOf = {};
+    data.forEach((item) => {
+      const { [idKey]: id, [parentKey]: parentId = 0 } = item;
+      childrenOf[id] = childrenOf[id] || [];
+      item[childrenKey] = childrenOf[id];
+      parentId
+          ? (
+              childrenOf[parentId] = childrenOf[parentId] || []
+          ).push(item)
+          : tree.push(item);
+    });
+    return tree;
+  }
+
+  $scope.commentList = []
+  $scope.flatCommentList = []
+  $scope.addComment = function(parentId = "") {
+    console.log($scope.commentValue, $scope.inputReply)
+    $scope.flatCommentList.push({ id: String($scope.counter) , content: $scope.commentValue, isTextareaShown: false, parent: String(parentId) })
+
+    console.table($scope.flatCommentList);
+    const commentsData = JSON.parse(JSON.stringify($scope.flatCommentList))
+    $scope.commentList = hierarchy(commentsData, {idKey: 'id', parentKey: 'parent'})
+    console.log($scope.commentList)
+    $scope.counter++
+  }
+
+  $scope.deleteComment = function (commentId) {
+    console.table($scope.flatCommentList)
+    $scope.flatCommentList.map((item, index, arr) => {
+      if (item.id === commentId || item.parent === commentId) {
+        arr.splice(index, 1);
+      }
+    })
+    console.table($scope.flatCommentList)
+    const commentsData = JSON.parse(JSON.stringify($scope.flatCommentList))
+    $scope.commentList = hierarchy(commentsData, {idKey: 'id', parentKey: 'parent'})
+    console.log($scope.commentList)
+// пройтись по всем комментам и когда найдется объект с нужным id удалить его
+  }
+}])
